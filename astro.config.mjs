83893ef1f52f9e7c590f3922a126d8fe4491eab4
Carlos Tocino <carlostocinocubelo@gmail.com>
2024-03-22 12:24:05 +0100

    v1.0 finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sharlibeicon.github.io'
+  site: 'https://sharlibeicon.github.io',
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false
+  }
+  }
 });
