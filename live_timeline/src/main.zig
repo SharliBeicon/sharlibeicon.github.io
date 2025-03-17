84f9d83cb730a43078bafb7caa5cf0009e88f7be
Charlie Bacon <ct@databeacon.aero>
2025-03-17 10:24:42 +0100

    adding some artworks

--- live_timeline/src/main.zig
+++ live_timeline/src/main.zig
@@ -9,8 +9,12 @@ const WindowSize = struct {
 
     fn get() WindowSize {
         const display = rl.getCurrentMonitor();
-        const width: i32 = @intFromFloat(@round(@as(f32, @floatFromInt(rl.getMonitorWidth(display))) * 0.8));
-        const height: i32 = @intFromFloat(@round(@as(f32, @floatFromInt(rl.getMonitorHeight(display))) * 0.75));
+        const width: i32 = @intFromFloat(
+            @round(@as(f32, @floatFromInt(rl.getMonitorWidth(display))) * 0.8),
+        );
+        const height: i32 = @intFromFloat(
+            @round(@as(f32, @floatFromInt(rl.getMonitorHeight(display))) * 0.75),
+        );
 
         return WindowSize{ .width = width, .height = height };
     }
@@ -18,23 +22,42 @@ const WindowSize = struct {
 
 pub fn main() anyerror!void {
     const window_size = WindowSize.get();
-    rl.initWindow(window_size.width, window_size.height, "raylib-zig [core] example - basic window");
+    rl.initWindow(window_size.width, window_size.height, "Charlie's Live Timeline");
     defer rl.closeWindow();
 
+    const banner_img = try rl.loadImage("resources/banner.png");
+    const banner = try rl.loadTextureFromImage(banner_img);
+    defer rl.unloadTexture(banner);
+    rl.unloadImage(banner_img);
+
+    const loop_args = LoopArgs{
+        .banner = banner,
+    };
+
     switch (builtin.os.tag) {
-        .emscripten => emscripten.emscripten_set_main_loop(&updateDrawFrame, 60, 1),
+        .emscripten => emscripten.emscripten_set_main_loop_arg(
+            &updateDrawFrame,
+            &loop_args,
+            60,
+            1,
+        ),
         else => {
+            rl.setConfigFlags(.{ .window_resizable = true });
             rl.setTargetFPS(60);
             while (!rl.windowShouldClose()) {
-                updateDrawFrame();
+                updateDrawFrame(&loop_args);
             }
         },
     }
 }
 
-pub fn updateDrawFrame() callconv(.c) void {
+const LoopArgs = struct { banner: rl.Texture2D };
+
+pub fn updateDrawFrame(args: *const LoopArgs) callconv(.c) void {
     const window_size = WindowSize.get();
-    rl.setWindowSize(window_size.width, window_size.height);
+    if (builtin.os.tag == .emscripten) {
+        rl.setWindowSize(window_size.width, window_size.height);
+    }
 
     rl.beginDrawing();
     defer rl.endDrawing();
@@ -44,5 +67,9 @@ pub fn updateDrawFrame() callconv(.c) void {
     const text = "Work in progress, come back later!!";
     const text_offset = @divTrunc(rl.measureText(text, 20), 2);
 
-    rl.drawText(text, @divTrunc(window_size.width, 2) - text_offset, @divTrunc(window_size.height, 2), 20, rl.Color.light_gray);
+    const banner_x = @divTrunc(window_size.width, 2) - @divTrunc(args.banner.width, 2);
+    const banner_y = @divTrunc(window_size.height, 2) - @divTrunc(args.banner.height, 2);
+
+    rl.drawTexture(args.banner, banner_x, banner_y, rl.Color.white);
+    rl.drawText(text, @divTrunc(window_size.width, 2) - text_offset, banner_y, 20, rl.Color.light_gray);
 }
